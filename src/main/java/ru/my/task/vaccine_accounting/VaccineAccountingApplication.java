1289d5443c32e6821db4cabe7408c615de521bad
Nikassa <julia.klimenko2602@gmail.com>
2020-06-16 00:10:10 +0300

    feat(front): Добавление Swagger, устранение несовместимости со SpringBoot #2

--- src/main/java/ru/my/task/vaccine_accounting/VaccineAccountingApplication.java
+++ src/main/java/ru/my/task/vaccine_accounting/VaccineAccountingApplication.java
@@ -2,8 +2,11 @@ package ru.my.task.vaccine_accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class VaccineAccountingApplication {
 
     public static void main(String[] args) {
