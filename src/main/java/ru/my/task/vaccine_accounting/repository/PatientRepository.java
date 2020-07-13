35d7e6b9b4ccda3afdfc0f2771e57295de84b770
Nikassa <julia.klimenko2602@gmail.com>
2020-07-13 09:30:09 +0300

    feat(back): Добавление testcontainers(in work) #103

--- src/main/java/ru/my/task/vaccine_accounting/repository/PatientRepository.java
+++ src/main/java/ru/my/task/vaccine_accounting/repository/PatientRepository.java
@@ -10,4 +10,8 @@ public interface PatientRepository extends JpaRepository<Patient, Integer> {
 //    @Query("SELECT p FROM Patient p WHERE p.id = :id")
 //    public Patient findPatientById( @Param("id") Integer id);
 //    public Patient findPatientById(int id);
+
+//    @Query("UPDATE Patient p SET p.name = 'lastname' WHERE p.id = :id")
+    @Query("UPDATE Patient p SET name = 'lastname' WHERE id = 1")
+    public int updatePatientNameById(@Param("id") Integer id);
 }
